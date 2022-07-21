4bd3d96d367455b80b2d6b127e3bb66a9a5d6365
DaxDB <lucassales.areas@gmail.com>
2022-07-21 14:01:45 -0300

    Dark Mode, Alto Contraste, Estrutura Inicial

---
+++ assets/js/darkmode.js
@@ -0,0 +1,84 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    colorText: getStyle(html, "--color-text"),
+    linkColor: getStyle(html, "--link-color"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    focus: getStyle(html, "--focus"),
+    theme: getStyle(html, "--bg-theme"),
+
+}
+
+const darkMode = {
+    bg: "#121212",
+    colorText: "#FFFFFF",
+    linkColor: "#b3b3b3",
+    colorHeadings: "#ffffff",
+    focus:"#FFFFFF",
+    theme: "#1F2022"
+    
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
+
+const isExistLocalStorage = (key) => 
+  localStorage.getItem(key) != null
+
+const createOrEditLocalStorage = (key, value) => 
+  localStorage.setItem(key, JSON.stringify(value))
+
+const getValeuLocalStorage = (key) =>
+  JSON.parse(localStorage.getItem(key))
+
+checkbox.addEventListener("change", ({target}) => {
+  if (target.checked) {
+    changeColors(darkMode) 
+    createOrEditLocalStorage('modo','darkMode')
+  } else {
+    changeColors(initialColors)
+    createOrEditLocalStorage('modo','initialColors')
+  }
+})
+
+if(!isExistLocalStorage('modo'))
+  createOrEditLocalStorage('modo', 'initialColors')
+
+
+if (getValeuLocalStorage('modo') === "initialColors") {
+  checkbox.removeAttribute('checked')
+  changeColors(initialColors);
+} else {
+  checkbox.setAttribute('checked', "")
+  changeColors(darkMode);
+}
+
+document.addEventListener("keypress", function(e) {
+  if(e.key === 'Enter') {
+  
+      var btn = document.querySelector("#switch");
+    
+    btn.click();
+  
+  }
+});
